77d8797acd11b4ecb359d5a718a0738963930197
Jonas Älmeblad <almeblad@gmail.com>
2017-10-06 10:57:01 +0000

    Working version that can update mondobd with latest value

---
+++ water-meter/update_mongodb_with_values.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+import datetime
+from pymongo import MongoClient
+
+if len(sys.argv) > 1:
+    amount = str(sys.argv[1])
+    client = MongoClient()
+    client = MongoClient('localhost', 27017)
+    db = client.water_meter
+
+    usage = {"amount": amount, "date": datetime.datetime.utcnow()}
+
+    usages = db.usages
+    usage_id = usages.insert_one(usage).inserted_id
+else:
+   sys.exit()
+
